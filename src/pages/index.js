99b934b1d60529ca56bdcda20bb41546cd5713cd
Daniel <grezzi@hotmail.com>
2023-03-25 20:16:02 +0000

    create basic structure nextJS

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Daniel G. Lima</title>
+        <meta name="description" content="Personal page for portifolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <main >
+        <h1>Hello World</h1>
+      </main>
+    </>
+  )
+}
