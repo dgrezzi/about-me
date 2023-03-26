b811f5e9670b65486abec3cce832ec573add7a47
Daniel <grezzi@hotmail.com>
2023-03-26 11:02:53 +0100

    separate scss archives

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/global.scss'
 import '../styles/style.scss'
+import '../styles/header.style.scss'
+import '../styles/main.style.scss'
+import '../styles/footer.style.scss'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
