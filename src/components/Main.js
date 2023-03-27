e4cfce5c09cc333fbcddeba287be428b7a856d6d
Daniel <grezzi@hotmail.com>
2023-03-27 10:22:18 +0100

    update avatar and name

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,13 @@
-import { useEffect, useState } from "react";
-
 export function Main() {
-    const [name, setName] = useState()
-    const [url, setUrl] = useState()
-
-    useEffect(() => {
-        fetch(`https://api.github.com/users/dgrezzi`)
-            .then(async res => {
-                const dados = await res.json();
-                setName(dados.name)
-                setUrl(dados.avatar_url)
-            });
-    }, []);
-
     return (
         <>
             <div className="container">
-                <img src={url} alt="Avatar" />
+                <img src="/avatar.jpg" alt="Avatar" />
                 <div className="description">
                     <h1>Hello!</h1>
                     <h2>About me:</h2>
                     <p className="indent">
-                        "My name is {name} and I'm a Brazilian living in West
+                        "My name is Daniel and I'm a Brazilian living in West
                         Sussex, England. Graduated in Chemistry, I have always had a
                         strong interest in programming. During college, I had the
                         opportunity to develop laboratory analysis equipment
