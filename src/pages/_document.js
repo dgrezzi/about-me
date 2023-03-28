ef309ee8a696589c5553d7a479484e636ea08c6e
Daniel <grezzi@hotmail.com>
2023-03-28 10:24:28 +0100

    add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
         <Html>
             <Head>
                 <title>Daniel G. Lima</title>
-
+                <link rel='shortcut icon' href='/favicon.svg' />
             </Head>
             <body>
                 <Main />
                 <NextScript />
             </body>
-
         </Html>
-    )
+    );
 }
